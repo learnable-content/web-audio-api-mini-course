32a215d137d39aed2ef74b861e223b793811f62c
James Wright <james@jamesswright.co.uk>
2016-08-24 22:45:12 +0100

    Boilerplate for effects episode

---
+++ web-audio-series/effects/boilerplate/index.js
@@ -0,0 +1,26 @@
+(function () {
+	'use strict';
+
+	const PIANO_URL = 'https://raw.githubusercontent.com/learnable-content/jamesseanwright/master/web-audio-series/files/piano.mp3';
+	const IR_URL = 'https://raw.githubusercontent.com/learnable-content/jamesseanwright/master/web-audio-series/files/ir.wav';
+
+	const context = new AudioContext();
+	
+	const bufferPromise = Promise.all([
+		downloadAsAudioBuffer(PIANO_URL),
+		downloadAsAudioBuffer(IR_URL)
+	]);
+
+	bufferPromise.then(play);
+
+	function downloadAsAudioBuffer(url) {
+		return window.fetch(url)
+				.then(response => response.arrayBuffer())
+				.then(arrayBuffer => context.decodeAudioData(arrayBuffer));
+	}
+
+	function play(audioBuffers) {
+		const pianoBuffer = audioBuffers[0];
+		const irBuffer = audioBuffers[1];
+	}
+}());
