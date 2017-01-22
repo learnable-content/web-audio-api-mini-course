aa618e2bda61b61a01c664635e9ccf085db8e64c
James Wright <james@jamesswright.co.uk>
2017-01-22 20:39:31 +0000

    Replace individual enqueue calls with arrays

--- complete/index.js
+++ complete/index.js
@@ -19,7 +19,7 @@
 		oscillator.start(time);
 	}
 
-	function enqueueBass(startTime, frequency) {
+	function enqueueBass([startTime, frequency]) {
 		const { currentTime } = audioContext;
 		const time = currentTime + startTime;
 		const oscillator = audioContext.createOscillator();
@@ -35,7 +35,7 @@
 		oscillator.start(time);
 	}
 
-	function enqueueLead(startTime, frequency) {
+	function enqueueLead([startTime, frequency]) {
 		const { currentTime } = audioContext;
 		const time = currentTime + startTime;
 		const oscillator = audioContext.createOscillator();
@@ -51,66 +51,76 @@
 		oscillator.stop(time + 0.1);
 	}
 
-	enqueueKickDrum(0);
-	enqueueKickDrum(0.5);
-	enqueueKickDrum(1);
-	enqueueKickDrum(1.5);
-	enqueueKickDrum(2);
-	enqueueKickDrum(2.5);
-	enqueueKickDrum(3);
-	enqueueKickDrum(3.5);
-	enqueueKickDrum(4);
-	enqueueKickDrum(4.5);
-	enqueueKickDrum(5);
-	enqueueKickDrum(5.5);
-	enqueueKickDrum(6);
-	enqueueKickDrum(6.5);
-	enqueueKickDrum(7);
-	enqueueKickDrum(7.5);
+	const kickSequence = [
+		0,
+		0.5,
+		1,
+		1.5,
+		2,
+		2.5,
+		3,
+		3.5,
+		4,
+		4.5,
+		5,
+		5.5,
+		6,
+		6.5,
+		7,
+		7.5
+	];
 
-	enqueueBass(0, 98);
-	enqueueBass(0.25, 98);
-	enqueueBass(1.25, 130.81);
-	enqueueBass(1.75, 123.47);
-	enqueueBass(2, 98);
-	enqueueBass(2.25, 98);
-	enqueueBass(3.25, 130.81);
-	enqueueBass(3.75, 123.47);
-	enqueueBass(4, 130.81);
-	enqueueBass(4.25, 130.81);
-	enqueueBass(5.25, 174.61);
-	enqueueBass(5.75, 164.81);
-	enqueueBass(6, 130.81);
-	enqueueBass(6.25, 130.81);
-	enqueueBass(7.25, 130.81);
-	enqueueBass(7.75, 123.47);
+	const bassSequence = [
+		[0, 98],
+		[0.25, 98],
+		[1.25, 130.81],
+		[1.75, 123.47],
+		[2, 98],
+		[2.25, 98],
+		[3.25, 130.81],
+		[3.75, 123.47],
+		[4, 130.81],
+		[4.25, 130.81],
+		[5.25, 174.61],
+		[5.75, 164.81],
+		[6, 130.81],
+		[6.25, 130.81],
+		[7.25, 130.81],
+		[7.75, 123.47]
+	];
 
-	enqueueLead(0, 392);
-	enqueueLead(0.5, 392);
-	enqueueLead(0.75, 392);
-	enqueueLead(0.9, 392);
-	enqueueLead(1.1, 392);
-	enqueueLead(1.25, 392);
-	enqueueLead(1.5, 440);
-	enqueueLead(1.75, 493.88);
-	enqueueLead(2, 392);
-	enqueueLead(2.5, 392);
-	enqueueLead(2.75, 392);
-	enqueueLead(2.9, 392);
-	enqueueLead(3.1, 392);
-	enqueueLead(3.25, 392);
-	enqueueLead(3.5, 440);
-	enqueueLead(3.75, 493.88);
-	enqueueLead(4, 392);
-	enqueueLead(4.5, 392);
-	enqueueLead(4.75, 392);
-	enqueueLead(4.9, 392);
-	enqueueLead(5.1, 392);
-	enqueueLead(5.25, 392);
-	enqueueLead(5.5, 440);
-	enqueueLead(5.75, 493.88);
-	enqueueLead(6, 523.25);
-	enqueueLead(6.5, 493.88);
-	enqueueLead(7, 440);
-	enqueueLead(7.5, 493.88);
+	const leadSequence = [
+		[0, 392],
+		[0.5, 392],
+		[0.75, 392],
+		[0.9, 392],
+		[1.1, 392],
+		[1.25, 392],
+		[1.5, 440],
+		[1.75, 493.88],
+		[2, 392],
+		[2.5, 392],
+		[2.75, 392],
+		[2.9, 392],
+		[3.1, 392],
+		[3.25, 392],
+		[3.5, 440],
+		[3.75, 493.88],
+		[4, 392],
+		[4.5, 392],
+		[4.75, 392],
+		[4.9, 392],
+		[5.1, 392],
+		[5.25, 392],
+		[5.5, 440],
+		[5.75, 493.88],
+		[6, 523.25],
+		[6.5, 493.88],
+		[7, 440],
+		[7.5, 493.88]
+	];
+
+	kickSequence.forEach(enqueueKickDrum);
+	bassSequence.forEach(enqueueBass);
+	leadSequence.forEach(enqueueLead);
 }());
