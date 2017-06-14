cae4b401bddbd487c3deda938d59f6ebeb307cd4
James Wright <james.wright@thetrainline.com>
2017-06-14 16:31:55 +0100

    Convert remaining indentation to spaces

--- boilerplate/index.js
+++ boilerplate/index.js
@@ -1,26 +1,26 @@
 (function () {
-	'use strict';
+    'use strict';
 
-	const { Keyboard, Drums, Recorder } = window.APP;
-	const context = new AudioContext();
+    const { Keyboard, Drums, Recorder } = window.APP;
+    const context = new AudioContext();
 
-	const keyboard = new Keyboard(
-		context,
-		document.body.querySelector('.keyboard'),
-		document.body.querySelector('#key-template')
-	);
+    const keyboard = new Keyboard(
+        context,
+        document.body.querySelector('.keyboard'),
+        document.body.querySelector('#key-template')
+    );
 
-	const drums = new Drums(
-		context,
-		document.body.querySelector('.drums'),
-		document.body.querySelector('#drum-loop-template')
-	);
+    const drums = new Drums(
+        context,
+        document.body.querySelector('.drums'),
+        document.body.querySelector('#drum-loop-template')
+    );
 
-	const recorder = new Recorder(
-		context,
-		document.body.querySelector('.recorder'),
-		document.body.querySelector('#recording-template'),
-		keyboard,
-		drums
-	);
+    const recorder = new Recorder(
+        context,
+        document.body.querySelector('.recorder'),
+        document.body.querySelector('#recording-template'),
+        keyboard,
+        drums
+    );
 }());
